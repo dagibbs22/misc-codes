d264bcd99b90e9cee3cbe931e78dadef26e6ca46
dagibbs22 <dagibbs22@gmail.com>
2018-07-13 15:53:39 -0400

    test

--- process_Woods_Hole_biomass.py
+++ process_Woods_Hole_biomass.py
@@ -1,6 +1,8 @@
 import subprocess
+import os
 
 def create_vrt():
+    print os.path.dirname(os.path.dirname(__file__))
     vrtname = 'carbon_v4.vrt'
     builtdvrt = ['gdalbuildvrt', vrtname, '../raw/*.tif']
     subprocess.check_call(builtdvrt)
